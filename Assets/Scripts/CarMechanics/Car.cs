e20434df990dfcc36aa51841e28061d5577c6d7e
hexidian <leokastenberg@gmail.com>
2020-07-10 19:01:39 -0400

    create car spawner

--- Assets/Scripts/CarMechanics/Car.cs
+++ Assets/Scripts/CarMechanics/Car.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class Car : MonoBehaviour
 {
 
+    public Transform endPoint;
+
+    private float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 10f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x )
+        transform.position = Vector3.MoveTowards(transform.position, endPoint.position, speed * Time.deltaTime);
+        if (transform.position == endPoint.position)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/CarMechanics/CarSpawn.cs
+++ Assets/Scripts/CarMechanics/CarSpawn.cs
@@ -5,24 +5,26 @@ using UnityEngine;
 public class CarSpawn : MonoBehaviour
 {
 
-    public int MaximumCars;
+    public GameObject SpawnItem;
+    public Transform endPoint;
 
-    int totalCars;
+    private float lastSpawnTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastSpawnTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (totalCars < MaximumCars)
+        float time = Time.time;
+        if(time - lastSpawnTime > 2)
         {
-            newCar = Instantiate(Car);
-            WaitForSeconds(5);
+            GameObject newCar = Instantiate(SpawnItem, transform.position, transform.rotation);
+            newCar.GetComponent<Car>().endPoint = endPoint;
+            lastSpawnTime = time;
         }
-        
     }
 }
