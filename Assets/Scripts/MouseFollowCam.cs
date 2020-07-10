b428393dc092d749f0abec3ca23e878d02c49897
printer83mph <printer.83mph@gmail.com>
2020-07-10 19:15:38 -0400

    Add MouseFollowCam

---
+++ Assets/Scripts/MouseFollowCam.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class MouseFollowCam : MonoBehaviour
+{
+
+    public float trackInfluence;
+
+    private Quaternion _initialRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _initialRotation = transform.rotation;
+    }
+
+    private void Update()
+    {
+        float mouseX = (Input.mousePosition.x / Screen.width - .5f) * trackInfluence;
+        float mouseY = (Input.mousePosition.y / Screen.height - .5f) * trackInfluence;
+        Quaternion desiredRotationAddition = Quaternion.Euler(-mouseY, mouseX, 0);
+
+        transform.rotation = _initialRotation * desiredRotationAddition;
+    }
+
+}
