5df96a9e42030efaf3202a8e1305597919351746
hexidian <leokastenberg@gmail.com>
2020-07-11 16:29:26 -0400

    add ability to restart level after failing

--- Assets/Scripts/EndDialogue.cs
+++ Assets/Scripts/EndDialogue.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndDialogue : MonoBehaviour
 {
 
     public GameObject dialogue;
-
     public IdlePoint[] postConvoTargetLocations;
+
+    public string failScene;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +30,11 @@ public class EndDialogue : MonoBehaviour
         {
             i.hasNoiseOn = true;
         }
+    }
+
+    public void failedConvo()
+    {
+        SceneManager.LoadScene(failScene);
     }
 
 }
