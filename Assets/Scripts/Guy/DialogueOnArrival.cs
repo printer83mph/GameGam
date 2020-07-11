cfff5b38a00c9603c0c336b02a6d3468c540fbe8
hexidian <leokastenberg@gmail.com>
2020-07-11 13:59:09 -0400

    canvas stuff

--- Assets/Scripts/Guy/DialogueOnArrival.cs
+++ Assets/Scripts/Guy/DialogueOnArrival.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 [RequireComponent(typeof(GuyMovement))]
 
-public class OnLocationEventTrigger : MonoBehaviour
+public class DialogueOnArrival : MonoBehaviour
 {
 
-    public IdlePoint onArrivalAt;
-    public IdlePoint eventAt;
+    public GameObject dialogue;
+    public IdlePoint arivalPoint;
 
     private GuyMovement mover;
 
     // Start is called before the first frame update
     void Start()
     {
+        dialogue.SetActive(false);
         mover = GetComponent<GuyMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!mover.isMoving() && mover.location == onArrivalAt)
+        if (!mover.isMoving() && mover.location == arivalPoint)
         {
-            eventAt.hasNoiseOn = true;
+            dialogue.SetActive(true);
         }
     }
 }
