cb5ba6fb00ab10cab54cdb605a0c99618e499cd5
hexidian <leokastenberg@gmail.com>
2020-07-11 23:42:12 -0400

    add cursor change on clickable objects

--- Assets/Scripts/Clickables/Clickable.cs
+++ Assets/Scripts/Clickables/Clickable.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.IO;
 
 public class Clickable : MonoBehaviour
 {
@@ -6,17 +8,33 @@ public class Clickable : MonoBehaviour
 
     public ClickDelegate onClickDelegate;
 
+    private Renderer rend;
+    private Color ogColor;
+
+    private Texture2D cursor;
+
     // Start is called before the first frame update
     void Start()
     {
         // give it our own method first (to avoid null pointer exception)
         onClickDelegate += OnClick;
 
+        cursor = new Texture2D(1, 1);//filler size. loading the image with change the size
+
+        cursor.LoadImage(File.ReadAllBytes("Assets/Cursor/cursor.png"));
+
     }
 
     private void OnMouseEnter()
     {
-        Debug.Log("entered");
+        Debug.Log("On it");
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("Off it");
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
 
     void OnMouseDown()
