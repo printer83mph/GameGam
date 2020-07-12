4d6ffe1209a3b4e5741f37145a6edffd28123dbd
hexidian <leokastenberg@gmail.com>
2020-07-12 01:56:16 -0400

    add scene 2 to 3 transition, add exit game menu, create new build

---
+++ Assets/Scripts/LevelManagement/ExitButtonOpener.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButtonOpener : MonoBehaviour
+{
+
+    public GameObject menu;
+
+    private bool _isActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu.SetActive(false);
+        _isActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            _isActive = !_isActive;
+            menu.SetActive(_isActive);
+        }
+    }
+}
