a01915b9078fcb9d63dca17a590ec44b74efd878
hexidian <leokastenberg@gmail.com>
2020-07-12 12:06:16 -0400

    add ending

---
+++ Assets/Scripts/EndingDeterminer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingDeterminer : MonoBehaviour
+{
+
+    public void GoodEnding()
+    {
+        SceneManager.LoadScene("GoodEnding");
+    }
+
+    public void BadEnding()
+    {
+        SceneManager.LoadScene("BadEnding");
+    }
+
+}
